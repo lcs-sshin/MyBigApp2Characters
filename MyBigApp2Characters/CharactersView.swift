6d07d0e9f54852a8da2affd4bcdf5add0d8d242f
Stella Shin <sshin@lcs.on.ca>
2026-03-02 12:22:00 -0500

    Segmented picker working List created. Haven't done divider yet

--- MyBigApp2Characters/CharactersView.swift
+++ MyBigApp2Characters/CharactersView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CharactersView: View {
     
+    @State private var selection = "History"
+    
     var body: some View {
         NavigationStack{
             VStack(spacing:10){
@@ -19,19 +21,111 @@ struct CharactersView: View {
                         Text("Harry Potter")
                             .font (.title2)
                         Text("Gryffindor")
+                        Image(systemName: "chevron.frontwards")
+                        
+                        
+                        
                     }
                     Spacer()
+                    
                 }
-                .frame(maxWidth:.infinity)
                 .padding()
-                .background(Color.yellow)
-                .foregroundStyle(Color.black)
+                
             }
+            VStack(spacing:10){
+                HStack{
+                    Rectangle()
+                        .frame(width:60,height:90)
+                    VStack(alignment:.leading, spacing:0){
+                        Text("Hermione Granger")
+                            .font (.title2)
+                        Text("Gryffindor")
+                        Image(systemName: "chevron.frontwards")
+                        
+                        
+                        
+                    }
+                    Spacer()
+                    
+                }
+                .padding()
+                
+            }
+            VStack(spacing:10){
+                HStack{
+                    Rectangle()
+                        .frame(width:60,height:90)
+                    VStack(alignment:.leading, spacing:0){
+                        Text("Ron Weasley")
+                            .font (.title2)
+                        Text("Gryffindor")
+                        Image(systemName: "chevron.frontwards")
+                        
+                        
+                        
+                    }
+                    Spacer()
+                    
+                }
+                .padding()
+                
+            }
+            VStack(spacing:10){
+                HStack{
+                    Rectangle()
+                        .frame(width:60,height:90)
+                    VStack(alignment:.leading, spacing:0){
+                        Text("Draco Malfoy")
+                            .font (.title2)
+                        Text("Slytherin")
+                        Image(systemName: "chevron.frontwards")
+                        
+                        
+                        
+                    }
+                    Spacer()
+                    
+                }
+                .padding()
+                
+            }
+            VStack(spacing:10){
+                HStack{
+                    Rectangle()
+                        .frame(width:60,height:90)
+                    VStack(alignment:.leading, spacing:0){
+                        Text("Minerva McGonagall")
+                            .font (.title2)
+                        Text("Gryffindor")
+                        Image(systemName: "chevron.frontwards")
+                        
+                        
+                        
+                    }
+                    Spacer()
+                    
+                }
+                .padding()
+                
+            }
+            
+            
+            Spacer()
                 .navigationTitle("Characters")
-                     
+            
         }
+        VStack{
+        }
+        .padding()
+        Spacer()
+        Picker("Current Selection",  selection: $selection ) {
+            Text ("Characters") .tag("Characters")
+            Text ("Favourites") .tag("Favourites")
+        }
+        .pickerStyle(.segmented)
     }
 }
+
 
 #Preview {
     CharactersView()
